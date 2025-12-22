78db3158a6ec4d3dccae43e10bb6c266705b9fd7
manu <git.manuel98@gmail.com>
2025-12-22 09:38:06 +0100

    feat: configure app core providers and global styles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,32 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideRouterStore } from '@ngrx/router-store';
 
 import { routes } from './app.routes';
+import { chatReducer } from './features/chat/store/chat.reducer';
+import { ChatEffects } from './features/chat/store/chat.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    }),
+    provideStore({ chat: chatReducer }),
+    provideEffects([ChatEffects]),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideRouterStore()
+]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'chat', pathMatch: 'full' },
+  { 
+    path: 'chat', 
+    loadComponent: () => import('./features/chat/pages/chat-page/chat-page.component').then(m => m.ChatPageComponent)
+  }
+];
